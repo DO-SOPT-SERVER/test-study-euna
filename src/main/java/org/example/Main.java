f804cee1a509178878a1e2c2e8d9f9ba85389650
sung-silver <jinna0319@gmail.com>
2024-02-25 00:53:46 +0900

    [DEL] 메인에서 사용하지 않는 코드 삭제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-        if(-1/2 < 0){
-            System.out.println("음수이다");
-        }
         System.out.println("Hello world!");
     }
 }
