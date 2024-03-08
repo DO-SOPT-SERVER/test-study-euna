5acd8c78ad8c1efb7db0a900dc9d081624beeb46
sung-silver <jinna0319@gmail.com>
2024-03-08 16:18:59 +0900

    [FIX] MemberProfileUpdateRequest를 record로 변경

--- spring-code-for-deploy/src/main/java/com/example/seminar/dto/request/member/MemberProfileUpdateRequest.java
+++ spring-code-for-deploy/src/main/java/com/example/seminar/dto/request/member/MemberProfileUpdateRequest.java
@@ -1,13 +1,9 @@
 package com.example.seminar.dto.request.member;
 
 import com.example.seminar.domain.Part;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
-@AllArgsConstructor
-public class MemberProfileUpdateRequest {
-    private short generation;
-    private Part part;
+public record MemberProfileUpdateRequest (
+        short generation,
+        Part part
+){
 }
-
