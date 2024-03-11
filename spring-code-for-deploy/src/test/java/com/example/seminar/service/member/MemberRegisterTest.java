aa04f27aa867125b0f51f9a511365e6f89c917e8
sung-silver <jinna0319@gmail.com>
2024-03-11 12:45:44 +0900

    [MODIFY] class 이름 변경 MemberSaver -> MemberRegister

--- spring-code-for-deploy/src/test/java/com/example/seminar/service/member/MemberRegisterTest.java
+++ spring-code-for-deploy/src/test/java/com/example/seminar/service/member/MemberRegisterTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class MemberSaverTest {
+class MemberRegisterTest {
 
     @Autowired
     private MemberRegister memberRegister;
